8b19a47401db58cc0b6f215abdd4f9c2aa676574
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2017-11-20 23:52:41 +0300

    Cosmetics.

--- Examples/SshKeyboardAuth/SshKeyboardAuth.cpp
+++ Examples/SshKeyboardAuth/SshKeyboardAuth.cpp
@@ -3,18 +3,17 @@
 
 using namespace Upp;
 
-
 CONSOLE_APP_MAIN
 {
-	const char *host = "test.rebex.net";	// A well-known public (S)Ftp test serverç
+	const char *host = "test.rebex.net";	// A well known public (S)FTP test server.
 	const char *user = "demo";
 	const char *pass = "password";
 	
 	Ssh::Trace();
 	SshSession session;
 	session.WhenKeyboard = [=](String title, String instructions, String prompt) mutable {
-		Cout() << prompt;		// Ignore title and instructions.
-		return ReadSecret();		// Rebex does not provide them anyway.
+        Cout() << prompt;       // Ignore title and instructions.
+        return ReadSecret();    // Rebex does not provide them anyway.
 	};
 	if(session.Timeout(30000).KeyboardAuth().Connect(host, 22, user, pass))
 		Cout() << "\nSuccessfully connected to " << host << ", using keyboard-interactive method.\n";
