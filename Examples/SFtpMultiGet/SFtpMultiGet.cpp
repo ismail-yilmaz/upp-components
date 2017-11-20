c1762de4db6c06707f3992c912a49fb29adf9572
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2017-11-21 01:53:14 +0300

    Example re-written.

--- Examples/SFtpMultiGet/SFtpMultiGet.cpp
+++ Examples/SFtpMultiGet/SFtpMultiGet.cpp
@@ -3,48 +3,67 @@
 
 using namespace Upp;
 
-// This example demonstrates downloading multiple files from a SFTP server asynchronously.
+// This example demonstrates getting a listing of directory,
+// and using the list to download multiple files from a SFTP server asynchronously.
 
-void GetRemoteFiles(ArrayMap<String, SFtp>& downloaders)
+const char *host  = "test.rebex.net";	// A well known public (S)FTP test server.
+const char *user  = "demo";
+const char *pass  = "password";
+const char *dir   = "/pub/example/";
+const int  MAXDOWNLOADS  = 4;
+
+void SetDownloaders(SshSession& ssh, const SFtp::DirList& ls, ArrayMap<String, SFtp>& dls)
 {
-	// Non-blocking downloader:
-	while(!downloaders.IsEmpty())
-		for(auto i = 0; i < downloaders.GetCount(); i++) {
-			SocketWaitEvent we;
-			auto& sftp = downloaders[i];
-			sftp.AddTo(we);
-			we.Wait(10);
-			if(!sftp.Do()) {
-				if(sftp.IsError())
-					Cerr() << sftp.GetErrorDesc() << '\n';
-				else {
-					auto fname = AppendFileName(
-						GetTempPath(),
-						GetFileName(downloaders.GetKey(i))
-					);
-					FileOut fout(fname);
-					if(fout) {
-						Cout() << "Writing the transferred data to " << fname << "...\n";
-						fout.Put((String) sftp.GetResult());
-					}
-					else Cerr() << "Unable to open local file " << fname << "!\n";
-				}
-				downloaders.Remove(i);
-				break;
-			}
-		}
+	for(auto i = 0; i < ls.GetCount(); i++) {
+		if(!ls[i].IsFile())
+			continue;
+		if(dls.GetCount() == MAXDOWNLOADS)
+			break;
+		auto  file = AppendFileName(dir, ls[i].GetName());
+		auto& sftp = dls.Add(file, new SFtp(ssh));
+		sftp.NonBlocking()
+		    .Timeout(0)
+		    .Get(file);
+		DLOG("Dowloading " << file);
+	}
 }
 
+void GetRemoteFiles(ArrayMap<String, SFtp>& dls)
+{
+	// Non-blocking downloader.
+	while(!dls.IsEmpty())
+		for(int i = 0; i < dls.GetCount(); i++) {
+			auto& sftp = dls[i];
+			auto  file = AppendFileName(GetTempPath(), GetFileName(dls.GetKey(i)));
+			SocketWaitEvent we;
+			sftp.AddTo(we);
+			we.Wait(10);
+			if(sftp.Do())
+				continue;
+			if(sftp.IsError()) {
+				DLOG("Unable to download " << dls.GetKey(i));
+				DLOG(sftp.GetErrorDesc());
+			}
+			else {
+				FileOut fout(file);
+				if(!fout) {
+					DLOG("Unable to write data to " << file);
+					DLOG(fout.GetErrorText());
+				}
+				else {
+					DLOG("Writing transferred data to " << file);
+					fout.Put(sftp.GetResult().To<String>());
+				}
+			}
+			dls.Remove(i);
+			break;
+		}
+}
 CONSOLE_APP_MAIN
 {
-	const char *host  = "test.rebex.net";	// A well known public (S)FTP test server.
-	const char *user  = "demo";
-	const char *pass  = "password";
-	const char *dir   = "/pub/example/";
-	const int  MAXDOWNLOADS  = 4;
+	StdLogSetup(LOG_COUT | LOG_FILE);
+//	Ssh::Trace();
 
-	
-	Ssh::Trace();
 	SshSession session;
 	if(session.Timeout(30000).Connect(host, 22, user, pass)) {
 		auto sftp = session.CreateSFtp();
@@ -53,18 +72,7 @@ CONSOLE_APP_MAIN
 			for(auto& e : ls)
 				Cout() << e << '\n';
 			ArrayMap<String, SFtp> downloaders;
-			for(auto i = 0; i < ls.GetCount(); i++) {
-				if(!ls[i].IsFile())
-					continue;
-				if(downloaders.GetCount() == MAXDOWNLOADS)
-					break;
-				auto  file = AppendFileName(dir, ls[i].GetName());
-				auto& sftp = downloaders.Add(file, new SFtp(session));
-				sftp.NonBlocking()
-				    .Timeout(0)
-				    .Get(file);
-				Cout() << "Downloading remote file " << file << "...\n";
-			}
+			SetDownloaders(session, ls, downloaders);
 			GetRemoteFiles(downloaders);
 		}
 		else Cerr() << sftp.GetErrorDesc() << '\n';
