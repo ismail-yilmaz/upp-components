cfac105fe7b89530ae7534b0259180a437c40923
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2018-01-25 23:39:16 +0300

    Typo fixed

--- Examples/SshShellGUI/SshShellGUI.cpp
+++ Examples/SshShellGUI/SshShellGUI.cpp
@@ -11,11 +11,10 @@ public:
 	SshConsole(SshSession& session) : SshShell(session) {
 		SetFont(Courier(14));
 		NonBlocking();
-		ChunkSize(1024*1024 * 10); // For better X11 tunneling experience.
-		RunWithX11("ansi", 0, Size(80, 24));
+		Run("ansi", 80, 24);
 
 		WhenOutput = [=](const void* b, int l) {
-			WString out((const char*) b, l);
+			String out((const char*) b, l);
 			if(out[0] == 0x08)
 				Backspace();
 			else
@@ -27,7 +26,7 @@ public:
 	bool Key(dword key, int count) {
 		switch(key) {
 			case K_BACKSPACE:
-				Send(0x08 & 0xff);
+				Send(0x08);
 				break;
 			case K_CTRL_C:
 				Send(0x03);
@@ -40,9 +39,6 @@ public:
 				break;
 			case K_F2:
 				Send("ls -l\r\n");
-				break;
-			case K_F3:
-				Send("export DISPLAY=:0\n");
 				break;
 			default:
 				if(key >= 0 && key < 65535)
@@ -58,44 +54,31 @@ public:
 };
 
 struct SshShellGUI : public TopWindow {
-	TabCtrl tabs;
-	
 	SshShellGUI() {
-		Title("A very simple SSH shell example (in X11 tunneling mode!) with GUI")
+		Title("A very simple SSH shell example with GUI")
 		.Sizeable()
 		.Zoomable();
 	}
 
 	void Run() {
-		const char *url = "maldoror:succubus@localhost:22"; // A well-known public SSH test server.
+		const char *url = "demo:password@test.rebex.net:22"; // A well-known public SSH test server.
 		SshSession session;
 		if(session.Connect(url)) {
-			Array<SshConsole> consoles;
-			auto& con1 = consoles.Add(new SshConsole(session));
-			auto& con2 = consoles.Add(new SshConsole(session));
-			
-			Add(tabs.SizePos());
-			tabs.Add(con1.SizePos(), "Ssh Console 1");
-			tabs.Add(con2.SizePos(), "Ssh Console 2");
+
+			SshConsole console(session);
+			Add(console.SizePos());
 
 			Open();
 			
-			while(!consoles.IsEmpty()) {
-				for(auto i = 0; i < consoles.GetCount(); i++) {
-					ProcessEvents();
-					if(!IsOpen())
-						return;
-					SocketWaitEvent we;
-					consoles[i].AddTo(we);
-					if(!consoles[i].Do()) {
-						if(consoles[i].IsError())
-							Exclamation(DeQtf(consoles[i].GetErrorDesc()));
-						consoles.Remove(i);
-						break;
-					}
-					we.Wait(10);
-				}
+			// Non-blocking event loop.
+			while(IsOpen() && console.Do()) {
+				ProcessEvents();
+				SocketWaitEvent we;
+				console.AddTo(we);
+				we.Wait(10);
 			}
+			if(console.IsError())
+				Exclamation(DeQtf(console.GetErrorDesc()));
 		}
 		else
 			Exclamation(DeQtf(session.GetErrorDesc()));
