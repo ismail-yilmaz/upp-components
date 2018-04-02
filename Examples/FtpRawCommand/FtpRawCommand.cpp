71b2325e3c3a55ca53663122882f85d41a9ab60f
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2018-04-02 14:22:08 +0300

    Ftp raw command example added.

---
+++ Examples/FtpRawCommand/FtpRawCommand.cpp
@@ -0,0 +1,18 @@
+#include <Core/Core.h>
+#include <FTP/Ftp.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_COUT|LOG_FILE);
+//	Ftp::Trace();
+
+	Ftp ftpclient;
+	if(ftpclient.Timeout(30000).Connect("demo:password@test.rebex.net:21")) {
+		ftpclient.SendCommand("HELP");
+		if(!ftpclient.IsError())
+			Cout() << ftpclient.GetReply();
+	}
+	LOG(ftpclient.GetErrorDesc());
+}
