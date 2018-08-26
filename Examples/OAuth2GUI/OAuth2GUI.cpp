dad73ad9758504b1b4ca49328aeae10a742ff297
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2018-08-27 02:08:35 +0300

    OAuth2 GUI example added.

---
+++ Examples/OAuth2GUI/OAuth2GUI.cpp
@@ -0,0 +1,73 @@
+#include <CtrlLib/CtrlLib.h>
+#include <ChromiumBrowser/ChromiumBrowser.h>
+#include <OAuth/OAuth.h>
+
+using namespace Upp;
+
+#define LAYOUTFILE <OAuth2GUI/OAuth2GUI.lay>
+#include <CtrlCore/lay.h>
+
+// Note: This authorization dialog can be called within any U++ GUI application.
+
+class GoogleDriveAuthDialog : public OAuth2Request, public TopWindow {
+	ChromiumBrowser browser; // We'll use the U++/CEF wrapper.
+public:
+	GoogleDriveAuthDialog()
+	{
+		SetRect(0, 0, 640, 540);
+		CenterScreen();
+		Title(t_("Google Drive Authorization"));
+		Add(browser.SizePos());
+	
+		WhenWait  = [=] { ProcessEvents(); };
+		WhenAuth  = [=](String url) { browser.StartPage("file://" + url); Open(); };
+		WhenClose = [=] { throw OAuth2Request::Error("Authentication cancelled."); };
+	}
+
+	bool Authorize(int port = 3245)
+	{
+		const char *CLIENT_ID      = "464598234891-8n18175koiqfjkpmm7bp11aukstn8rc1.apps.googleusercontent.com";
+		const char *CLIENT_SECRET  = "gVUutg7282BSzDg7dFYGyopV";
+		const char *CLIENT_SCOPE   = "https://www.googleapis.com/auth/drive";
+		const char *AUTH_ENDPOINT  = "https://accounts.google.com/o/oauth2/auth";
+		const char *TOKEN_ENDPOINT = "https://www.googleapis.com/oauth2/v4/token";
+		
+		Port(port);
+		UseNonce();
+		Credentials(CLIENT_ID, CLIENT_SECRET);
+	
+		if(GetAuthCode(OAuth2CodeRequest(AUTH_ENDPOINT, CLIENT_SCOPE), code))
+			GetAccessToken(OAuth2TokenRequest(TOKEN_ENDPOINT, code), response);
+		if(IsOpen())
+			Close();
+		return !IsError();
+	}
+
+	String	code;
+	String	response;
+};
+
+struct OAuth2App : public WithMainLayout<TopWindow> {
+	GoogleDriveAuthDialog gauth;
+	OAuth2App()
+	{
+		CtrlLayout(*this, t_("OAuth2 Test"));
+		auth.WhenAction = [=]
+		{
+			if(gauth.Authorize())
+				PromptOK(DeQtf(gauth.response));
+			else ErrorOK(DeQtf(gauth.GetErrorDesc()));
+		};
+	}
+};
+
+GUI_APP_MAIN
+{
+	StdLogSetup(LOG_FILE);
+	SetLanguage(SetLNGCharset(GetSystemLNG(), CHARSET_UTF8));
+	if (ChromiumBrowser::IsChildProcess()){
+		ChromiumBrowser::ChildProcess();
+	}else{
+		OAuth2App().Run();
+	}
+}
