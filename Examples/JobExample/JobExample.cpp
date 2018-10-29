46e3fb88688b6e27c529c4161edf432377ead5b7
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2018-10-30 01:07:12 +0300

    Job: Reference example added.

---
+++ Examples/JobExample/JobExample.cpp
@@ -0,0 +1,39 @@
+#include <Core/Core.h>
+#include <Job/Job.h>
+
+using namespace Upp;
+
+CONSOLE_APP_MAIN
+{
+	StdLogSetup(LOG_FILE|LOG_COUT);
+	
+	Job<void>   job1;
+	Job<String> job2;
+	
+	{
+		job1.Do([=]{ LOG("Hello world!"); });
+		job1.Get();
+	}
+	{
+		job1.Do([=]{ throw Exc("Error"); });
+		try {
+			job1.Get();
+		}
+		catch(...) {
+			LOG("Exception caught!");
+		}
+	}
+	{
+		job1.Do([=]{
+			while(!Job<String>::IsCancelled())
+				Sleep(1);
+			LOG("Worker canceled.");
+		});
+		Sleep(1000);
+		// job2.Cancel(); // Worker will automatically cancel the job when it goes out of scope.
+	}
+	{
+		job2.Do([=]{ return "Hello again!"; });
+		LOG(~job2);
+	}
+}
