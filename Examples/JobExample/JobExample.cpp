4307a547596ddd9e16448267f1ad8198c290be38
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2018-10-31 14:47:05 +0300

    SshBasic: A reference example set, demonstrating the basic features of SSH pacakge

--- Examples/JobExample/JobExample.cpp
+++ Examples/JobExample/JobExample.cpp
@@ -7,33 +7,27 @@ CONSOLE_APP_MAIN
 {
 	StdLogSetup(LOG_FILE|LOG_COUT);
 	
-	Job<void>   job1;
-	Job<String> job2;
+	{
+		Job<String> job1;
+		job1.Do([=]{ return "Hello world!"; });
+		RLOG(~job1);
+	}
 	
-	{
-		job1.Do([=]{ LOG("Hello world!"); });
-		job1.Get();
+	Job<void> job2;
+
+	job2.Do([=]{ throw Exc("Error"); });
+	try {
+		job2.Get();
 	}
-	{
-		job1.Do([=]{ throw Exc("Error"); });
-		try {
-			job1.Get();
-		}
-		catch(...) {
-			LOG("Exception caught!");
-		}
+	catch(...) {
+		RLOG("Exception caught!");
 	}
-	{
-		job1.Do([=]{
-			while(!Job<void>::IsCancelled())
-				Sleep(1);
-			LOG("Worker canceled.");
-		});
-		Sleep(1000);
-		//job1.Cancel(); // Worker will automatically cancel the job when it goes out of scope.
-	}
-	{
-		job2.Do([=]{ return "Hello again!"; });
-		LOG(~job2);
-	}
+
+	job2.Do([=]{
+		while(!Job<void>::IsCanceled())
+			Sleep(1);
+		RLOG("Job2 is canceled.");
+	});
+	Sleep(1000);
+	// job2.Cancel(); // Worker will automatically cancel the job when it goes out of scope.
 }
