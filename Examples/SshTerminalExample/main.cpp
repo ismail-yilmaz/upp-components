8c6151853fce3ce5518897f32f2a50680162776e
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2019-07-20 16:18:05 +0300

    Termina: Examples added.

---
+++ Examples/SshTerminalExample/main.cpp
@@ -0,0 +1,49 @@
+#include <Core/Core.h>
+#include <Core/SSH/SSH.h>
+#include <Terminal/Terminal.h>
+
+using namespace Upp;
+
+String url = "demo:password@test.rebex.net:22";	// A well-known public SSH test server.
+
+struct SshTerminal : Terminal, SshShell {
+	SshTerminal(SshSession& session) : SshShell(session)
+	{
+		SshShell::Timeout(Null);
+		SshShell::ChunkSize(65536);
+		SshShell::WhenOutput = [=](const void *data, int size) { Terminal::Write(data, size); };
+		Terminal::WhenOutput = [=](String data) { SshShell::Send(data); };
+		Terminal::WhenResize = [=]() { SshShell::PageSize(Terminal::GetPageSize()); };
+	}
+
+	void Run(const String& termtype)
+	{
+		SshShell::Run(termtype, Terminal::GetPageSize());
+		if(SshShell::IsError())
+			ErrorOK(DeQtf(GetErrorDesc()));
+	}
+};
+
+struct SshTerminalExample : TopWindow {
+	void Run()
+	{
+		if(!EditTextNotNull(url, "SSH server", "Url"))
+			return;
+		SshSession session;
+		session.WhenWait = [=]{ ProcessEvents(); };
+		if(!session.Timeout(10000).Connect(url)) {
+			ErrorOK(DeQtf(session.GetErrorDesc()));
+			return;
+		}
+		SshTerminal term(session);
+		SetRect(term.GetStdSize()); // 80 x 24 cells (scaled)
+		Sizeable().Zoomable().CenterScreen().Add(term.SizePos());
+		OpenMain();
+		term.Run("xterm");
+	}
+};
+
+GUI_APP_MAIN
+{
+	SshTerminalExample().Run();
+}
---
+++ Examples/TerminalExample/main.cpp
@@ -0,0 +1,39 @@
+#include <CtrlLib/CtrlLib.h>
+#include <Terminal/Terminal.h>
+#include <Terminal/PtyProcess.h>
+
+using namespace Upp;
+
+const char *nixshell = "/bin/bash";
+
+struct TerminalExample : TopWindow {
+	Terminal  term;
+	PtyProcess pty;					// This class is completely optional
+	
+	TerminalExample()
+	{
+		SetRect(term.GetStdSize());	// 80 x 24 cells (scaled)
+		Sizeable().Zoomable().CenterScreen().Add(term.SizePos());
+
+		term.WhenBell   = [=]()			{ BeepExclamation(); };
+		term.WhenTitle  = [=](String s)	{ Title(s);	};
+		term.WhenResize = [=]()			{ pty.SetSize(term.GetPageSize()); };
+		term.WhenOutput = [=](String s)	{ PutGet(s); };
+
+		SetTimeCallback(-1, [=] { PutGet(); });
+		pty.Start(nixshell, Environment(), GetHomeDirectory()); // Defaults to TERM=xterm
+	}
+	
+	void PutGet(String out = Null)
+	{
+		term.CheckWriteUtf8(pty.Get());
+		pty.Write(out);
+		if(!pty.IsRunning())
+			Break();
+	}
+};
+
+GUI_APP_MAIN
+{
+	TerminalExample().Run();
+}
