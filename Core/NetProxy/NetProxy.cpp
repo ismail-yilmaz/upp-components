d39078cdf336ce1ac49f6a70b69c092a040b5aab
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2020-06-10 00:37:37 +0300

    NetProxy: Commented HTTP_CONNECT sample commentused for testing/debugging is removed.

--- Core/NetProxy/NetProxy.cpp
+++ Core/NetProxy/NetProxy.cpp
@@ -218,13 +218,6 @@ void NetProxy::HttpcConnect()
 
 bool NetProxy::HttpcRequest()
 {
-// CONNECT www.microsoft.com:443 HTTP/1.0
-// User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko
-// Host: www.microsoft.com
-// Content-Length: 0
-// DNT: 1
-// Connection: Keep-Alive
-// Pragma: no-cache
 	LLOG("Starting HTTP_CONNECT tunneling...");
 	packet.Clear();
 	packet_length = 0;
