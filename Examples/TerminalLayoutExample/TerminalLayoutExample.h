f84842c93447b20b7a3d26cce48517aa9e554cfc
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2021-01-23 03:09:09 +0300

    Examples: TerminalLayoutExample is added to the examples. (Demonstrates custom gui building.)

---
+++ Examples/TerminalLayoutExample/TerminalLayoutExample.h
@@ -0,0 +1,47 @@
+#ifndef _TerminalLayoutExample_TerminalLayoutExample_h
+#define _TerminalLayoutExample_TerminalLayoutExample_h
+
+#include <CtrlLib/CtrlLib.h>
+#include <Terminal/Terminal.h>
+
+// This example demonstrates a simple, cross-platform (POSIX/Windows)
+// terminal example.
+
+// On Windows, the PtyProcess class requires at least Windows 10 (tm)
+// for the new pseudoconsole API support. To enable this feature, you
+// need to set the WIN10 flag in TheIDE's main package configurations
+// dialog. (i.e. "GUI WIN10")
+
+using namespace Upp;
+
+#define LAYOUTFILE <TerminalLayoutExample/TerminalLayoutExample.lay>
+#include <CtrlCore/lay.h>
+
+class TerminalExample : public WithTerminalExampleLayout<TopWindow> {
+public:
+	TerminalExample();
+	void Serialize(Stream& s) override;
+	void Run();
+	
+private:
+	enum class WindowOp : int {
+		FullScreen,
+		Maximize,
+		Minimize,
+		Resize,
+		Geometry
+	};
+	
+	void	WindowAction(WindowOp action, Value arg = Null);
+	void	MainMenu(Bar& bar);
+	void	FileMenu(Bar& bar);
+	void	ViewMenu(Bar& bar);
+	void	ContextMenu(Bar& bar);
+	
+private:
+	PtyProcess pty;
+	MenuBar mainmenu;
+	StatusBar statusbar;
+};
+
+#endif
---
+++ Examples/TerminalLayoutExample/main.cpp
@@ -0,0 +1,148 @@
+// This example demonstrates a simple, cross-platform (POSIX/Windows)
+// terminal example.
+
+// On Windows, the PtyProcess class requires at least Windows 10 (tm)
+// for the new pseudoconsole API support. To enable this feature, you
+// need to set the WIN10 flag in TheIDE's main package configurations
+// dialog. (i.e. "GUI WIN10")
+
+#include "TerminalLayoutExample.h"
+
+#ifdef PLATFORM_POSIX
+const char *tshell = "/bin/bash";
+#elif  PLATFORM_WIN32
+const char *tshell = "cmd.exe";  // Alternatively, you can use powershell...
+#endif
+
+TerminalExample::TerminalExample()
+{
+	CtrlLayout(*this, "Terminal Layout Example");
+	AddFrame(mainmenu);
+	AddFrame(statusbar);
+	mainmenu.Set([=](Bar& bar) { MainMenu(bar); });
+	Sizeable().Zoomable().SetRect(AddFrameSize(term.GetStdSize()));
+	term.WhenBell                 = [=]()                { BeepExclamation(); };
+	term.WhenBar                  = [=](Bar& bar)        { ContextMenu(bar);  };
+	term.WhenTitle                = [=](String s)        { Title(s);          };
+	term.WhenOutput               = [=](String s)        { pty.Write(s);      };
+	term.WhenResize               = [=]()                { pty.SetSize(term.GetPageSize()); };
+	term.WhenLink                 = [=](const String& s) { PromptOK(DeQtf(s));              };
+	term.WhenWindowMinimize       = [=](bool b)          { WindowAction(WindowOp::Minimize, b);   };
+	term.WhenWindowMaximize       = [=](bool b)          { WindowAction(WindowOp::Maximize, b);   };
+	term.WhenWindowFullScreen     = [=](int i)           { WindowAction(WindowOp::FullScreen, i); };
+	term.WhenWindowGeometryChange = [=](Rect r)          { WindowAction(WindowOp::Geometry, r);   };
+}
+
+void TerminalExample::Serialize(Stream& s)
+{
+	// Binary serialization support. It is also possible to use JSON or XML for persistence.
+
+	term.Serialize(s);
+	SerializePlacement(s);
+}
+
+void TerminalExample::Run()
+{
+	// Custon (high performance) event loop.
+
+	pty.Start(tshell, Environment(), GetHomeDirectory());
+	OpenMain();
+	while(IsOpen() && pty.IsRunning()) {
+		ProcessEvents();
+		String s = pty.Get();
+		int    l = s.GetLength();
+		term.WriteUtf8(s);
+		Sleep(l >= 1024 ? 1024 * 10 / l : 10); // Scale to workload.
+	}
+}
+
+void TerminalExample::WindowAction(WindowOp action, Value arg)
+{
+	// xterm's window ops.
+
+	if(IsFullScreen() && (action != WindowOp::FullScreen))
+		FullScreen(false);
+
+	switch(action) {
+	case WindowOp::Geometry:
+		SetRect(arg);
+		break;
+	case WindowOp::Minimize:
+		IsMinimized() ? Overlap() : Minimize();
+		break;
+	case WindowOp::Maximize:
+		IsMaximized() ? Overlap() : Maximize();
+		break;
+	case WindowOp::Resize:
+		SetRect(Rect(GetRect().TopLeft(), AddFrameSize(arg)));
+		break;
+	case WindowOp::FullScreen:
+		FullScreen(!int(arg) ? !IsFullScreen() : int(arg) > 0);
+		break;
+	default:
+		return;
+	}
+	
+	term.SetFocus();
+}
+
+void TerminalExample::MainMenu(Bar& bar)
+{
+	bar.Sub(t_("File"), [=](Bar& bar)    { FileMenu(bar); });
+	bar.Sub(t_("Edit"), [=](Bar& bar)    { term.EditBar(bar); });
+	bar.Sub(t_("View"), [=](Bar& bar)    { ViewMenu(bar); });
+	bar.Sub(t_("Options"), [=](Bar& bar) { term.OptionsBar(bar); });
+}
+
+void TerminalExample::FileMenu(Bar& bar)
+{
+	bar.Add(t_("Exit"), [=] { Close(); });
+}
+
+void TerminalExample::ViewMenu(Bar& bar)
+{
+	// Custom menu to define and toggle custom terminal display sizes.
+
+	bar.Add(t_("Toggle full screen"),
+		[=]{ WindowAction(WindowOp::FullScreen, 0); })
+		.Key(K_SHIFT_CTRL_F7)
+		.Check(IsFullScreen());
+	bar.Add(t_("Maximize window"),
+		[=]{ WindowAction(WindowOp::Maximize); })
+		.Key(K_SHIFT_CTRL_F6)
+		.Check(IsMaximized());
+	bar.Add(t_("Minimize window"),
+		[=]{ WindowAction(WindowOp::Minimize); })
+		.Key(K_SHIFT_CTRL_F5)
+		.Check(IsMinimized());
+	bar.Separator();
+	bar.Add("80 x 24",
+		[=]{ WindowAction(WindowOp::Resize, term.PageSizeToClient(80, 24));  })
+		.Key(K_SHIFT_CTRL_F1);
+	bar.Add("80 x 48",
+		[=]{ WindowAction(WindowOp::Resize, term.PageSizeToClient(80, 48));  })
+		.Key(K_SHIFT_CTRL_F2);
+	bar.Add("132 x 24",
+		[=]{ WindowAction(WindowOp::Resize, term.PageSizeToClient(132, 24)); })
+		.Key(K_SHIFT_CTRL_F3);
+	bar.Add("132 x 48",
+		[=]{ WindowAction(WindowOp::Resize, term.PageSizeToClient(132, 48)); })
+		.Key(K_SHIFT_CTRL_F4);
+}
+
+void TerminalExample::ContextMenu(Bar& bar)
+{
+	// Prepends the custom view menu to TerminalCtrl's standard menu.
+
+	bar.Sub(t_("View"), [=](Bar& bar) { ViewMenu(bar); });
+	bar.Separator();
+	term.StdBar(bar);
+}
+
+GUI_APP_MAIN
+{
+	TerminalExample upp_term;
+	LoadFromFile(upp_term);
+	upp_term.Run();
+	StoreToFile(upp_term);
+}
